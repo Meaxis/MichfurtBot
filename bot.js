707cacb1b285de42e94e711411373a9159238c46
Meaxis <37759268+Meaxis@users.noreply.github.com>
2018-06-15 20:19:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,15 +6,5 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-
-  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
-
-  client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setActivity(`This is the Michfurt Bot!`);
-});
   
 bot.login(tokenfile.token);
